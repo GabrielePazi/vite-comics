46012932364fc3374defb0a6cb27797c292b02d7
GabrielePazi <pazigabriele@gmail.com>
2023-08-22 19:16:59 +0200

    1.0 header

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+    components: {
+      TheHeader,
+      TheFooter
+    }
+  }
+  import TheHeader from './components/TheHeader.vue';
+  import TheFooter from './components/TheFooter.vue'
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  
+  <main class="bg-black">
+    <div class="container">
+      <h1 class="text-white py-5 ps-5">The content goes here</h1>
+    </div>
+  </main>
+
+  <TheFooter></TheFooter>
+</template>
+
+<style>
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,35 @@
+<script>
+  export default {
+    
+  };
+</script>
+
+<template>
+  <header>
+    <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <img class="h-100" src="../assets/images/dc-logo.png" alt="">
+      </a>
+
+      <ul class="nav nav-pills align-items-center">
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">CHARACTERS</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">COMICS</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">MOVIES</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">TV</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">GAMES</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">COLLECTIBLES</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">VIDEOS</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">FANS</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">NEWS</a></li>
+        <li class="nav-item"><a href="#" class="nav-link text-black fw-semibold">SHOP</a></li>
+      </ul>
+    </header>
+  </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
